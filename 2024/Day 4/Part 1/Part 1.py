c32d1027b776586feff0aab5de5c10377a512f87
Nuño <nunocambero@gmail.com>
2024-12-04 17:06:44 +0100

    Day 4

---
+++ 2024/Day 4/Part 1/Part 1.py
@@ -0,0 +1,26 @@
+def find_word(grid, word):
+    def search(x, y, dx, dy):
+        for k in range(len(word)):
+            if not (0 <= x + k * dx < len(grid) and 0 <= y + k * dy < len(grid[0])):
+                return False
+            if grid[x + k * dx][y + k * dy] != word[k]:
+                return False
+        return True
+
+    directions = [(1, 0), (0, 1), (1, 1), (1, -1), (-1, 0), (0, -1), (-1, -1), (-1, 1)]
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            for dx, dy in directions:
+                if search(i, j, dx, dy):
+                    count += 1
+    return count
+
+file = open("C:\\Users\\Universidad\\Advent of Code\\2024\\Day 4\\input.txt")
+lines = file.read().split("\n")
+count = 0
+
+grid = [list(line) for line in lines]
+
+print(find_word(grid, "XMAS"))
+file.close()
---
+++ 2024/Day 4/Part 2/Part 2.py
@@ -0,0 +1,32 @@
+def find_x_mas(grid):
+    rows, cols = len(grid), len(grid[0])
+    count = 0
+  
+    patterns = [
+        ('M', 'A', 'S', 'M', 'S'),
+        ('S', 'A', 'M', 'S', 'M'),
+        ('M', 'A', 'S', 'S', 'M'),
+        ('S', 'A', 'M', 'M', 'S')
+    ]
+
+    for r in range(1, rows - 1):
+        for c in range(1, cols - 1):
+            for p in patterns:
+                if (
+                    grid[r - 1][c - 1] == p[0] and
+                    grid[r][c] == p[1] and
+                    grid[r + 1][c + 1] == p[2] and
+                    grid[r - 1][c + 1] == p[3] and
+                    grid[r + 1][c - 1] == p[4]
+                ):
+                    count += 1
+                    break
+
+    return count
+
+with open("C:\\Users\\Universidad\\Advent of Code\\2024\\Day 4\\input.txt") as file:
+    lines = file.readlines()
+
+word_search = [list(line) for line in lines]
+
+print(find_x_mas(word_search))
